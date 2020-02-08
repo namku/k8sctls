d5ebf4837a6acbbeed0fa3bfac8701ec3e8bc4c6
namku <syak7771@gmail.com>
2020-02-08 21:26:38 +0100

    Add flaks in eks command

--- cmd/eks.go
+++ cmd/eks.go
@@ -2,8 +2,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 
 	"github.com/spf13/cobra"
@@ -11,6 +13,27 @@ import (
 
 func init() {
 	rootCmd.AddCommand(eksCmd)
+
+	eksCmd.PersistentFlags().String("duration-seconds", "86400", "Time in seconds until reset credentials")
+	eksCmd.PersistentFlags().String("serial-number", "", "arn user name")
+	eksCmd.MarkPersistentFlagRequired("serial-number")
+	eksCmd.PersistentFlags().StringP("token-code", "t", "", "two factor authentication code")
+	eksCmd.MarkPersistentFlagRequired("token-code")
+	eksCmd.PersistentFlags().String("profile", "", "account profile")
+	eksCmd.MarkPersistentFlagRequired("profile")
+}
+
+// struct credentials
+type Credentials struct {
+	AcessKeyId      string
+	SecretAccessKey string
+	SessionToken    string
+	Expiration      string
+}
+
+// struct identification
+type Identification struct {
+	Credentials Credentials
 }
 
 var eksCmd = &cobra.Command{
@@ -18,13 +41,34 @@ var eksCmd = &cobra.Command{
 	Short: "Configure credentials of eks cluster",
 	Long:  `Import configuration of eks cluster in .kube/config file.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Importing credentials..")
-		// aws sts get-session-token --duration-seconds $(durationSeconds) --serial-number $(serialNumber)  --token-code $(tokenCode) --profile $(profile)
-		list, err := exec.Command("aws", "sts", "get-session-token", "--duration-seconds", "86400").Output()
-		// err := list.Run()
+
+		ds, _ := cmd.Flags().GetString("duration-seconds")
+		sn, _ := cmd.Flags().GetString("serial-number")
+		tc, _ := cmd.Flags().GetString("token-code")
+		pf, _ := cmd.Flags().GetString("profile")
+
+		fmt.Println("aws sts get-session-token --duration-seconds " + ds + " --serial-number " + sn + " --token-code " + tc + " --profile " + pf)
+		secretAccessKey, err := exec.Command("aws", "sts", "get-session-token", "--duration-seconds", ds, "--serial-number", sn, "--token-code", tc, "--profile", pf).Output()
 		if err != nil {
 			log.Fatal("command failed", err)
 		}
-		fmt.Printf("%s", list)
+
+		// read output json format
+		var crd Identification
+		json.Unmarshal([]byte(secretAccessKey), &crd)
+
+		fmt.Printf("%+v", crd.Credentials.SecretAccessKey)
+		fmt.Printf("%+v", crd.Credentials.SessionToken)
+
+		// set environment vars
+		os.Setenv("AWS_SECRET_ACCESS_KEY", crd.Credentials.SecretAccessKey)
+		os.Setenv("AWS_SECRET_ACCESS_KEY", crd.Credentials.SessionToken)
+
+		// create context configuration eks cluster
+		awsContext := exec.Command("aws", "eks", "--region", "eu-central-1", "update-kubeconfig", "--name", "koble-stg-eks", "--profile", "nexus")
+		err = awsContext.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
